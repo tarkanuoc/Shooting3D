50992eb80c6db81126231aa8ef22142dcd71f741
Vu Hoang Anh <hoanganhgmd@gmail.com>
2022-12-03 15:31:06 +0700

    + Add zombie Attack

---
+++ Assets/Scripts/Enemy/ZombieAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttack : MonoBehaviour
+{
+    public Animator anim;
+    public int damage;
+    public Health playerHealth;
+
+    public void StartAttack()
+    {
+        anim.SetBool("IsAttacking", true);
+    }
+
+    public void StopAttack()
+    {
+        anim.SetBool("IsAttacking", false);
+    }
+
+    public void OnAttack()
+    {
+        playerHealth.TakeDamage(damage);
+    }
+}
